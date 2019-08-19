801e1dd62b0d84c59e89b87e04f5e2e71a81d9e3
Trent Walkup <trentonwalkup@legalshieldcorp.com>
2019-08-19 09:22:44 -0500

    fixes

--- tests/task5.js
+++ tests/task5.js
@@ -5,6 +5,7 @@ const driver = new Builder().forBrowser('chrome').build();
 describe('Task5 - selenium node js', () => {
   it('return correct number of profile images', () => {
     let test = driver.get('http://the-internet.herokuapp.com/dynamic_content')
+    .then(elm=> expect(elm.toString()).to.equal(3))
       return test
   });
   afterEach(()=> driver.quit())
